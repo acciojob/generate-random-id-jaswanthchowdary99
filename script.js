0d906f24cebf8d7ff40b28c74638fe406603dc0c
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-08 10:55:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
   // write your code here
+    let res = "";
+	let charList ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for(let i = 0; i < l; i++){
+		res += charList.charAt(Math.floor(Math.random() * charList.length));
+	}
+	return res;
 }
 
 // Do not change the code below.
